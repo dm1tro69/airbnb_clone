fa91e7557668a835a925f8d9f62a449f40106363
dm1tro69 <dimolg22@gmail.com>
2020-09-08 21:01:46 +0300

    add Modal

---
+++ src/pages/Login/SignUp.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import {bindActionCreators} from "redux";
+import openModal from "../../actions/openModal";
+import Login from "./Login";
+
+class SignUp extends Component{
+
+    render(){
+        return(
+            <div className="login-form">
+                <form onSubmit={this.submitLogin}>
+                    <button className="facebook-login">Connect With Facebook</button>
+                    <button className="google-login">Connect With Google</button>
+                    <div className="login-or center">
+                        <span>or</span>
+                        <div className="or-divider"></div>
+                    </div>
+                    <button className="sign-up-button">Sign up with email</button>
+                    <div className="divider"></div>
+                    <div>Already have an account? <span onClick={()=>this.props.openModal('open', <Login/>)}>Log in</span> </div>
+                </form>
+            </div>
+
+        )
+    }
+
+}
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        openModal: openModal
+    }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SignUp);
