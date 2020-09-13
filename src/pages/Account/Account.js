1760cb360ec395add5970d776b099406eb1958f4
dm1tro69 <dimolg22@gmail.com>
2020-09-13 13:37:40 +0300

    add Search

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -5,6 +5,8 @@ import axios from 'axios'
 import moment from 'moment'
 import {Route} from 'react-router-dom'
 import AccountSidebar from "./AccountSideBar";
+import Bookings from "./Bookings";
+import ChangePassword from "./ChangePassword";
 
 class Account extends Component{
 
@@ -14,13 +16,47 @@ class Account extends Component{
     }
 
    async componentDidMount() {
+        const accountUrl = `${window.apiHost}/users/getBookings`
+       const data = {
+            token: this.props.auth.token,
+
+       }
+       const resp = await axios.post(accountUrl, data)
+       let pastBookings = [], upcomingBookings = []
+       resp.data.forEach((booking)=>{
+           const today = moment()
+           const checkOutDate = moment(booking.checkOut)
+           const diffDays = checkOutDate.diff(today, 'days')
+           if(diffDays < 0){
+               pastBookings.push(booking)
+           }else {
+               upcomingBookings.push(booking)
+           }
+       })
+       this.setState({
+           pastBookings,
+           upcomingBookings
+       })
 
     }
 
     render() {
+        const {upcomingBookings, pastBooking} = this.state
         return(
             <div className={'account container-fluid'}>
                <AccountSidebar/>
+               <div className={'row'}>
+                   <div className={'col s8 offset-s3'}>
+                       <Route exact path={'/account'} render={()=> {
+                           return <h1>Chose an option on the left</h1>
+                       }}/>
+                       <Route exact path={'/account/reservations/confirmed'} render={()=>
+                          <Bookings type={'upcoming'} bookings={upcomingBookings} token={this.props.auth.token}/>
+                       }/>
+                       <Route exact path={'/account/reservations/past'} ><Bookings type={'past'} bookings={pastBooking}/></Route>
+                       <Route exact path={'/account/change-pass'} component={ChangePassword}/>
+                   </div>
+               </div>
             </div>
         )
     }
---
+++ src/pages/Account/ChangePassword.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ChangePassword = () => {
+    return(
+        <h1>Change Password</h1>
+    )
+}
+export default ChangePassword
